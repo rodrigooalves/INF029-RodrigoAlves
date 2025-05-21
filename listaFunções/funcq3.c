96c80443ae20492b9865692a8a334ed372dd96a2
Rodrigo Alves <119018810+rodrigooalves@users.noreply.github.com>
2025-05-20 22:33:04 -0300

    q3

--- listaFunções/funcq3.c
+++ listaFunções/funcq3.c
@@ -1,12 +1,25 @@
 #include <stdio.h>
 
-int fatorial (int a) {
-    return 
-} 
+int fatorial (int n) {
+    if (n < 0){
+        return -1;
+    }
+    
+    int resultado = 1;
+    for ( int i = n; i >= 1; i-- ){ 
+        resultado *= i;
+    }
+    return resultado;
+}
 
 int main (){
 
+int fat;
 
+    printf("Digite um numero para saber seu fatorial: \n");
+    scanf("%d", &fat);
+    
+    printf("Fatorial de %d e %d", fat, fatorial(fat));
 
-    return 0;
+return 0;
 }
