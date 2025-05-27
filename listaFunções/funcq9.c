ba7c8a7b1838a77ab8208a177829a0cb2fca8816
Rodrigo Alves <119018810+rodrigooalves@users.noreply.github.com>
2025-05-27 19:56:27 -0300

    go

--- listaFunções/funcq9.c
+++ listaFunções/funcq9.c
@@ -9,20 +9,39 @@ typedef struct {
 typedef struct {
     char nome[20];
     Data dataNascimento;
-    int cpf;
+    char cpf[15];
     char sexo;
 } Cliente;
 
 void cadastrarCliente (Cliente cliente[1] ) {
     
     printf("Cadastrando cliente: \n");
-    printf("Nome: ");
+    printf("Nome: \n");
     scanf("%s", cliente[0].nome);
+    getchar();
+    printf("Digite a data de nascimento '( 00 00 0000)':  ");
+    scanf("%d %d %d", &cliente[0].dataNascimento.dia,&cliente[0].dataNascimento.mes,&cliente[0].dataNascimento.ano);
+    printf("Digite o cpf 0000000000\n");
+    scanf("%s", cliente[0].cpf);
+    printf("Sexo [M/F]\n");
+    getchar();
+    scanf("%c", &cliente[0].sexo);
+}
 
+int validaNome (Cliente cliente[1]) {
+    
+}
+void printCliente (Cliente cliente[1]) {
+    printf("Nome: %s - Data de nascimento: %d/%d/%d - CPF: %s, Sexo: %c", 
+        cliente[0].nome, cliente[0].dataNascimento.dia,
+        cliente[0].dataNascimento.mes,cliente[0].dataNascimento.ano,
+        cliente[0].cpf,cliente[0].sexo);
 }
 
 int main () {
 
-
+    Cliente cliente[1];
+    cadastrarCliente(cliente);
+    printCliente(cliente);
 return 0;
 }
