8128e1818041e8366dd215d10b5ba0eb17fdd481
Rodrigo Alves <119018810+rodrigooalves@users.noreply.github.com>
2025-05-28 17:03:15 +0000

    go

---
+++ TRAINNINGC/questao1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int quadnmr(int n) {
+    printf("Digite o numero para descobrir seu quadrado: \n");
+    scanf("%d", &n);
+    n *= n;
+    printf("%d\n", n);
+    return n;
+}
+
+int main () {
+    int resultado = quadnmr(resultado);
+
+}
